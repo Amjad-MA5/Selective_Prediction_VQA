1ccfb09233f7c568d70c8331a572c3b441ed62c8
Ansh Prakash <anshprakash1997@gmail.com>
2024-07-06 13:36:39 +0000

    fix some issue import related issues

--- dataset/download_VQAv2_dataset.py
+++ dataset/download_VQAv2_dataset.py
@@ -1,5 +1,7 @@
 from datasets import load_dataset
 
+import sys
+sys.path.insert(0, "/teamspace/studios/this_studio/Selective_Prediction_PathVQA/dataset/download_VQAv2_dataset.py")
 
 def download_dataset(folder_path):
     dataset = load_dataset('VQAv2.py', name='v2')
--- dataset/get_VQAv2_dataset.py
+++ dataset/get_VQAv2_dataset.py
@@ -1,17 +1,23 @@
 from datasets import load_dataset
 import os
 
-folder_path = './data/VQAv2.hf'
-
+home_dir = "/teamspace/studios/this_studio"
+folder_path =  home_dir + "/Selective_Prediction_PathVQA/dataset/data/VQAv2.hf"
+script_path = home_dir + "/Selective_Prediction_PathVQA/dataset/VQAv2.py"
 
 # Ensure the dataset script file is in your PYTHONPATH or specify the path to the script
+
 def get_VQAv2_dataset():
+    print(folder_path)
     if os.path.isdir(folder_path):
         print(f"Loading from local path {folder_path}.")
-        dataset = load_dataset('VQAv2.py', data_dir=folder_path, name='v2')
+        dataset = load_dataset(script_path, data_dir=folder_path, name='v2')
     else:
-        print(f"Downloading from source")    
-        from download_VQAv2_dataset import download_dataset
+        print(f"Downloading from source")
+        try:
+            from download_VQAv2_dataset import download_dataset
+        except:
+            pass
         dataset = download_dataset(folder_path)
         
     return dataset
